5ee3c75d78b115188ae5cdbced04c9d734a6f244
AnEvilPenguin <17614766+AnEvilPenguin@users.noreply.github.com>
2024-04-08 18:37:15 +0100

    feat: initial build work

---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: {
+        app: "./src/index.js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            title: "EPs TODO App",
+
+            filename: "index.html",
+            inject: "head",
+            scriptLoading: "defer",
+        }),
+    ],
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
