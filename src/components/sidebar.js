76eb03172d062948be929424cb979325b3944188
AnEvilPenguin <17614766+AnEvilPenguin@users.noreply.github.com>
2024-04-12 20:37:54 +0100

    feat: add new-project button

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,9 +1,11 @@
 import { div, ul, li } from "../util/dom";
+import { Action } from "./action";
 
 export function Sidebar(project) {
   return div(
     "sidebar",
     {},
     ul("project-list", {}, li("project-item", {}, project.name)),
+    Action({ additionalClasses: ["new-project"], text: "New Project" }),
   );
 }
