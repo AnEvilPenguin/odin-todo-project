e8b7b6230959ce0377b42e189492f0b1fe4ebef7
AnEvilPenguin <17614766+AnEvilPenguin@users.noreply.github.com>
2024-04-12 20:36:30 +0100

    feat: add new todo button

--- src/components/content.js
+++ src/components/content.js
@@ -1,7 +1,14 @@
 import { div } from "../util/dom";
+import { Action } from "./action";
 import { ActionBar } from "./actionBar";
 import { Table } from "./table";
 
 export function Content(project) {
-  return div("content", {}, ActionBar(), Table(project));
+  return div(
+    "content",
+    {},
+    ActionBar(),
+    Table(project),
+    Action({ additionalClasses: ["new-button"], text: "New" }),
+  );
 }
