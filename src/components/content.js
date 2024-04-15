8846fd037795ae75b03fa8255c328372baf40276
AnEvilPenguin <17614766+AnEvilPenguin@users.noreply.github.com>
2024-04-15 17:15:17 +0100

    feat: basic new todo template

--- src/components/content.js
+++ src/components/content.js
@@ -1,14 +1,29 @@
+import { Todo } from "../classes/todo";
 import { div } from "../util/dom";
 import { Action } from "./action";
 import { ActionBar } from "./actionBar";
 import { Table } from "./table";
 
 export function Content(project, regenerateApp) {
+  const todoList = project.listTodoItems();
+
+  const newButton = Action({ additionalClasses: ["new-button"], text: "New" });
+  newButton.addEventListener("click", () => {
+    const newItem = new Todo(
+      `TODO-${todoList.length}`,
+      new Date().toISOString(),
+      "P1",
+    );
+
+    project.setTodoItem(newItem);
+    regenerateApp();
+  });
+
   return div(
     "content",
     {},
     ActionBar(),
     Table(project, regenerateApp),
-    Action({ additionalClasses: ["new-button"], text: "New" }),
+    newButton,
   );
 }
